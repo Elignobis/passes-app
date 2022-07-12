5503a40fc267ad26fab23826f21f87d93bddc5ad
Sibongile <rwayisibongile@gmail.com>
2022-07-12 12:27:49 +0200

    added pages

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
+    <router-link to="/players">Players</router-link> |
+    <router-link to="/admin">Admin</router-link> |
+    <router-link to="/contact">Contact</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
--- src/router/index.js
+++ src/router/index.js
@@ -10,10 +10,22 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/players',
+    name: 'players',
+    component: () => import('../views/Players.vue')
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: () => import('../views/Admin.vue')
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: () => import('../views/Contact.vue')
   }
 ]
 
